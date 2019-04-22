a77c9259bcf22af35a22f9d7a2624d85ee68ad85
rizky andre wibisono <rzkandre@gmail.com>
2019-04-22 13:01:52 +0700

    init

---
+++ AFSHiaAP.c
@@ -0,0 +1,22 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+
+static const char *dirpath = "/home";
+
+static struct fuse_operations xmp_oper = {
+  .getattr = xmp_getattr,
+  .readdir = xmp_readdir,
+  .read = xmp_read,
+};
+
+int int main(int argc, char const *argv[]) {
+  umask(0);
+  return fuse_main(argc, argv, &xmp_oper, NULL);
+}
